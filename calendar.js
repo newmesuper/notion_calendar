8d959937ce5855202f16f640f4ffc0183e7fbfef
newmesuper <121206210+newmesuper@users.noreply.github.com>
2025-05-13 17:59:12 +0900

    Update calendar.js
    선택된 날짜 표시 제거

--- calendar.js
+++ calendar.js
@@ -2,7 +2,7 @@ let display = document.querySelector('.display'),
     prevBtn = document.querySelector('.left'),
     nxtBtn = document.querySelector('.right'),
     days = document.querySelector('.days'),
-    selected = document.querySelector('.selected');
+    // selected = document.querySelector('.selected');
 
 let dateToday = new Date();
 let year = dateToday.getFullYear();
@@ -50,17 +50,17 @@ function displayCalendar(){
 }
 
 displayCalendar()
-displaySelected()
+// displaySelected()
 
-function displaySelected () {
-    const daysEL = document.querySelectorAll('.days div');
-    daysEL.forEach(day => {
-        day.addEventListener('click', (e) => {
-        const selectedDate = e.target.dataset.date;
-        selected.innerText = `선택일 : ${selectedDate}`;
-        })
+// function displaySelected () {
+//     const daysEL = document.querySelectorAll('.days div');
+//     daysEL.forEach(day => {
+//         day.addEventListener('click', (e) => {
+//         const selectedDate = e.target.dataset.date;
+//         selected.innerText = `선택일 : ${selectedDate}`;
+//         })
         
-    });
+//     });
 }
     prevBtn.addEventListener('click', () =>{
         days.innerHTML = '';
@@ -72,7 +72,7 @@ function displaySelected () {
         month = month -1;
         dateToday.setMonth(month);
         displayCalendar();
-        displaySelected();
+        // displaySelected();
     });
 
      nxtBtn.addEventListener('click', () =>{
@@ -85,5 +85,5 @@ function displaySelected () {
         month = month +1;
         dateToday.setMonth(month);
         displayCalendar();
-        displaySelected();
+        // displaySelected();
     });
